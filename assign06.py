cd856f838dd7acb3019830350892ee2dba4e0a81
AliAhmad-0 <aliahmad554327@gmail.com>
2024-09-16 15:32:58 +0500

    list in python

---
+++ assign06.py
@@ -0,0 +1,43 @@
+listOfNumber = [10,92,83,40]
+studentName = ["ali","zain","ahmad","bilal","hassan"]
+
+
+
+output = studentName[0:-3]
+print(output)
+
+nestedStudent = ["ali","waqas", ["bilal","abdullah"]]
+output = nestedStudent[2]
+print(output[0])
+
+output = ['hi'] * 4
+print(output)
+
+output = len(studentName)
+
+output = studentName.copy()
+
+output = studentName + listOfNumber
+
+output = studentName.clear()
+
+output = studentName.reverse()
+
+output = studentName.sort()
+
+output = studentName.count("ali")
+
+
+output = studentName.index("ali")
+
+output = studentName.remove("ali")
+
+result = studentName.append("awais")
+
+output = studentName.pop()
+
+output1 = studentName.extend(listOfNumber )
+
+output = studentName.insert(5, "awais")
+
+                
